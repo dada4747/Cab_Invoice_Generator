e416f35135abb7942655d0af440d160d11b3601b
dada4747 <adsurerahul96@gmail.com>
2021-06-21 06:33:32 +0530

    ADDED:Test Total Flare of Juourney

--- src/main/java/com/bridgelabz/CabInvoiceService.java
+++ src/main/java/com/bridgelabz/CabInvoiceService.java
@@ -1,5 +1,13 @@
 package com.bridgelabz;
 
 public class CabInvoiceService {
+    public static final double MAXMUM_FLARE = 5;
+    public static final double PER_KILOMETER_COST = 10;
+    public static final int PER_MINUTE_COST = 1;
+
+    public double calculatorFare( double distance, int time) {
+        double totalFare = distance * PER_KILOMETER_COST + time * PER_MINUTE_COST;
+        return Math.max(MAXMUM_FLARE, totalFare);
+    }
 
 }
--- src/test/java/com/bridgelabz/CabInvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceServiceTest.java
@@ -1,4 +1,23 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CabInvoiceServiceTest {
+    CabInvoiceService cabInvoiceService = new CabInvoiceService();
+
+    @Test
+    public void givenDistanceAndTime_shouldReturn_totalFare(){
+        double  distance = 5.0;
+        int time = 15;
+        double fare = cabInvoiceService.calculatorFare(distance, time);
+        Assertions.assertEquals(65,fare,0.0);
+    }
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        double distance = 0.1;
+        int time = 2;
+        double fare = cabInvoiceService.calculatorFare(distance, time);
+        Assertions.assertEquals(5, fare, 0.0);
+    }
 }
